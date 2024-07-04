304715c69ff19f7bc4bc2ecb56b5cc4936c97e50
HAPPYCODE <151997185+CODEPAGEHERE@users.noreply.github.com>
2024-07-04 11:55:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("reveal-button");
     const content = document.getElementById("content");
+    const audio = document.getElementById("birthday-song");
 
     button.addEventListener("click", () => {
         content.classList.toggle("hidden");
+        audio.play();
     });
 });
